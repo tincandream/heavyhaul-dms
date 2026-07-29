e2303380cdcb87d1fe1391fa75fed6eae813eede
tincandream <diannavmwaka@gmail.com>
2026-07-29 11:42:32 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "PASTE_YOUR_PROJECT_URL_HERE";
+const SUPABASE_URL = "https://zyhdatteglxozsgnjhrm.supabase.co/rest/v1/";
 
-const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5aGRhdHRlZ2x4b3pzZ25qaHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyODEzMTAsImV4cCI6MjEwMDg1NzMxMH0.I4SlAWiqKk0oPv-0WRgVy_AAGnQ0NoT8-T8z62jIj7Y";
 
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
